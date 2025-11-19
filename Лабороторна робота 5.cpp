1a295117fb66de987fb7ada8788e77e796715c4d
“Arlexs” <arlexsi270@gmail.com>
2025-11-19 17:22:06 +0200

    первый коммит

---
+++ Лабороторна робота 5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char ch;
+    cout << "Введіть дужку: ";
+    cin >> ch;
+
+
+    switch (ch) {
+    case '(':
+        cout << "Відкрита кругла дужка" << endl;
+        break;
+    case ')':
+        cout << "Закрита кругла дужка" << endl;
+        break;
+    case '[':
+        cout << "Відкрита квадратна дужка" << endl;
+        break;
+    case ']':
+        cout << "Закрита квадратна дужка" << endl;
+        break;
+    case '{':
+        cout << "Відкрита фігурна дужка" << endl;
+        break;
+    case '}':
+        cout << "Закрита фігурна дужка" << endl;
+        break;
+    default:
+        cout << "Це не дужка" << endl;
+    }
+
+    return 0;
+}
